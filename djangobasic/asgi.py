ccf08e243ff60464b447299e8ac32f38b265ddd7
AndreiGosovich <andrei_gosovich@mail.ru>
2022-11-20 15:13:14 +0300

    Фксация проекта по заданию 2

--- djangobasic/asgi.py
+++ djangobasic/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobasic.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangobasic.settings")
 
 application = get_asgi_application()
