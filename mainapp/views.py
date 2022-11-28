5c98fc3ced87828f9c16c4b04b2cc817429752ad
AndreiGosovich <andrei_gosovich@mail.ru>
2022-11-28 22:39:14 +0300

    Домашнее задание номер 3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+import json
+from django.shortcuts import redirect
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
@@ -9,6 +11,30 @@ class MainPageView(TemplateView):
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['news_title'] = 'заголовок новости'
+        context['news_text'] = 'Новость ни о чём. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, est rerum minus cum consequuntur totam dolores adipisci quaerat praesentium iusto?'
+        context['range'] = range(5)
+
+        with open('./static/News.json', 'r', encoding='utf-8') as news_file:
+            context['news_json'] = json.load(news_file)
+
+        return context
+
+class NewsPageViewPaginator(NewsPageView):
+
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page'] = page
+        return context
+
+
+class NewsPageViewSearch(NewsPageView):
+
+    def redirectToSearch(request, text):
+        return redirect(f'https://yandex.ru/search/?text={text}')
+        
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
