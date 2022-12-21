7356709cd06b6b649b60ada599b9360d9c2d9d9c
AndreiGosovich <andrei_gosovich@mail.ru>
2022-12-21 19:43:24 +0300

    Homework 9

--- djangobasic/celery.py
+++ djangobasic/celery.py
@@ -1,9 +1,10 @@
 import os
-
+from django.conf import settings
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangobasic.settings")
+if settings.DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangobasic.settings")
 
-celery_app = Celery("braniac")
-celery_app.config_from_object("django.conf:settings", namespace="CELERY")
-celery_app.autodiscover_tasks()
+    celery_app = Celery("braniac")
+    celery_app.config_from_object("django.conf:settings", namespace="CELERY")
+    celery_app.autodiscover_tasks()
